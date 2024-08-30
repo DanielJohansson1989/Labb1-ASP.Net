00b2b717703ae9688b0a7e2e15629897e10ec427
Daniel Johansson <dannesnake@gmail.com>
2024-08-30 15:00:29 +0200

    Create model for book

---
+++ LibraryAPI/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryAPI.Models
+{
+    public class Book
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required, MaxLength(50)]
+        public string Title { get; set; }
+        [Required, MaxLength(50)]
+        public string Author { get; set; }
+        public DateOnly DateOfPublication { get; set; }
+        public string Genre { get; set; }
+        public bool Available { get; set; }
+    }
+}
