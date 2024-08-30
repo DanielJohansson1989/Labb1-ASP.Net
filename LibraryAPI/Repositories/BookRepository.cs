bf5660adc79d8992f00aef3d0580d987d3a05978
Daniel Johansson <dannesnake@gmail.com>
2024-08-30 15:32:11 +0200

    Create repository for Books to use DbContext

---
+++ LibraryAPI/Repositories/BookRepository.cs
@@ -0,0 +1,49 @@
+﻿using LibraryAPI.Data;
+using LibraryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly AppDbContext _context;
+        public BookRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateBookAsync(Book book)
+        {
+            _context.Books.Add(book);
+        }
+
+        public async Task DeleteBookAsync(Book book)
+        {
+            _context.Books.Remove(book);
+        }
+
+        public async Task<IEnumerable<Book>> GetAllAsync()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        public async Task<Book> GetSingleAsync(int id)
+        {
+            return await _context.Books.FirstOrDefaultAsync(b => b.ID == id);
+        }
+
+        public async Task<Book> GetSingleAsync(string title)
+        {
+            return await _context.Books.FirstOrDefaultAsync(b => b.Title.ToLower() == title.ToLower());
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateBookAsync(Book book)
+        {
+            _context.Books.Update(book);
+        }
+    }
+}
---
+++ LibraryAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using LibraryAPI.Models;
+
+namespace LibraryAPI.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetAllAsync();
+        Task<Book> GetSingleAsync(int id);
+        Task<Book> GetSingleAsync(string title);
+        Task CreateBookAsync(Book book);
+        Task DeleteBookAsync(Book book);
+        Task UpdateBookAsync(Book book);
+        Task SaveAsync();
+    }
+}
