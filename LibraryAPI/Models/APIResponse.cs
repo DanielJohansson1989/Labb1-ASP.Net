4a0556c608c37ca665412981a2a4473bba8bca41
Daniel Johansson <dannesnake@gmail.com>
2024-09-01 10:38:46 +0200

    Create class for sending a response as a single object over Http

---
+++ LibraryAPI/Models/APIResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace LibraryAPI.Models
+{
+    public class APIResponse
+    {
+        public bool IsSuccess { get; set; }
+        public Object Result { get; set; }
+        public HttpStatusCode StattusCode { get; set; }
+        public List<string> ErrorMessages { get; set; }
+
+        public APIResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
+    }
+}
