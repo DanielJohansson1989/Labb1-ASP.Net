076999b8156789a82aa3768da0369ad49a9d74b6
Daniel Johansson <dannesnake@gmail.com>
2024-09-01 10:35:33 +0200

    Create a DTO for creating new book entry

---
+++ LibraryAPI/Models/DTOs/CreateBookDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryAPI.Models.DTOs
+{
+    public class CreateBookDTO
+    {
+        [Required, MaxLength(50)]
+        public string Title { get; set; }
+        [Required, MaxLength(50)]
+        public string Author { get; set; }
+        public DateOnly DateOfPublication { get; set; }
+        public string Genre { get; set; }
+    }
+}
