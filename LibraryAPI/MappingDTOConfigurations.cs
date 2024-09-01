f6efea6ab47eef2c78e64c4e44978beb0acfaa8f
Daniel Johansson <dannesnake@gmail.com>
2024-09-01 10:51:29 +0200

    Create configuration for automapper Automapper maps Book model to CreateBookDTO and vice versa. ID and Available properties not necessary when creating a new book entry in database.

---
+++ LibraryAPI/MappingDTOConfigurations.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LibraryAPI.Models;
+using LibraryAPI.Models.DTOs;
+
+namespace LibraryAPI
+{
+    public class MappingDTOConfigurations : Profile
+    {
+        public MappingDTOConfigurations() 
+        {
+            CreateMap<Book, CreateBookDTO>().ReverseMap();
+        }
+    }
+}
