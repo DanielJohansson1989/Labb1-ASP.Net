7795326127282167bb74474de065e68301eb04e0
Daniel Johansson <dannesnake@gmail.com>
2024-09-09 12:08:13 +0200

    Add data annotations

--- LibraryAPI/Models/DTOs/CreateBookDTO.cs
+++ LibraryAPI/Models/DTOs/CreateBookDTO.cs
@@ -4,9 +4,7 @@ namespace LibraryAPI.Models.DTOs
 {
     public class CreateBookDTO
     {
-        [Required, MaxLength(50)]
         public string Title { get; set; }
-        [Required, MaxLength(50)]
         public string Author { get; set; }
         public DateOnly DateOfPublication { get; set; }
         public string Genre { get; set; }
